9375bc524476c40cda944161c8415efc15e38d57
vishu247 <156403229+vishu247@users.noreply.github.com>
2024-05-30 14:49:44 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,36 +11,36 @@ echo $CASBIN_DB
 echo "ORCHESTRATOR_DB-----"
 echo $ORCHESTRATOR_DB
 
+export PGPASSWORD=$DB_PASSWORD
+
 echo "deleting and creating new DB"
 SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'orchestrator';
 
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $LENS_DB FROM public; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "ALTER DATABASE $LENS_DB  allow_connections = off; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname '$LENS_DB';"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$LENS_DB' AND pid <> pg_backend_pid();"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $LENS_DB ;"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $LENS_DB ;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $GIT_SENSOR_DB FROM public;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "ALTER DATABASE $GIT_SENSOR_DB ALLOW_CONNECTIONS = off;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$GIT_SENSOR_DB' AND pid <> pg_backend_pid();"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "DROP DATABASE $GIT_SENSOR_DB;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -t -c "CREATE DATABASE $GIT_SENSOR_DB;"
 
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $GIT_SENSOR_DB FROM public; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "ALTER DATABASE $GIT_SENSOR_DB  allow_connections = off; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname '$GIT_SENSOR_DB';"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$GIT_SENSOR_DB' AND pid <> pg_backend_pid();"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $GIT_SENSOR_DB ;"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $GIT_SENSOR_DB ;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $GIT_SENSOR_DB FROM public;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "ALTER DATABASE $GIT_SENSOR_DB ALLOW_CONNECTIONS = off;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$GIT_SENSOR_DB';"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$GIT_SENSOR_DB' AND pid <> pg_backend_pid();"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $GIT_SENSOR_DB;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $GIT_SENSOR_DB;"
 
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $CASBIN_DB FROM public; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "ALTER DATABASE $CASBIN_DB  allow_connections = off; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname '$CASBIN_DB';"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$CASBIN_DB' AND pid <> pg_backend_pid();"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $CASBIN_DB ;"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $CASBIN_DB ;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $CASBIN_DB FROM public;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "ALTER DATABASE $CASBIN_DB ALLOW_CONNECTIONS = off;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '$CASBIN_DB';"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$CASBIN_DB' AND pid <> pg_backend_pid();"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $CASBIN_DB;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $CASBIN_DB;"
 
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $ORCHESTRATOR_DB FROM public; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "ALTER DATABASE $ORCHESTRATOR_DB  allow_connections = off; "
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname '$ORCHESTRATOR_DB';"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$ORCHESTRATOR_DB' AND pid <> pg_backend_pid();"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $ORCHESTRATOR_DB ;"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $ORCHESTRATOR_DB ;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "REVOKE CONNECT ON DATABASE $ORCHESTRATOR_DB FROM public;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "ALTER DATABASE $ORCHESTRATOR_DB ALLOW_CONNECTIONS = off;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$ORCHESTRATOR_DB' AND pid <> pg_backend_pid();"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "DROP DATABASE $ORCHESTRATOR_DB;"
+psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "CREATE DATABASE $ORCHESTRATOR_DB;"
 
 PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d $DB_NAME -t -c "\l"
 
